4dc41d716d9be591484a4b53f7dc12cae989250e
Robera Geleta <robera.geleta@vensi.com>
2014-11-18 22:32:11 -0600

    Initial Commit

---
+++ RGSliderMoveController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RGSliderMoveController
+//
+//  Created by ROBERA GELETA on 11/18/14.
+//  Copyright (c) 2014 ROBERA GELETA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RGSliderMoveControllerTests/RGSliderMoveControllerTests.m
@@ -0,0 +1,40 @@
+//
+//  RGSliderMoveControllerTests.m
+//  RGSliderMoveControllerTests
+//
+//  Created by ROBERA GELETA on 11/18/14.
+//  Copyright (c) 2014 ROBERA GELETA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface RGSliderMoveControllerTests : XCTestCase
+
+@end
+
+@implementation RGSliderMoveControllerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
